37ad2444d81e57ef8400303605114185351eb9a1
shawnebaby <129100566+shawnebaby@users.noreply.github.com>
2026-06-24 07:51:42 -0500

    XT Resource
    -Added XT prog bar to hud for away ship
    -Added logic to player to charge while in a nether zone and expend xt when not in one

--- code/gameplay/objects/draw_nether_field.gd
+++ code/gameplay/objects/draw_nether_field.gd
@@ -2,4 +2,4 @@ extends CollisionShape2D
 
 func _draw() -> void:
 	if shape:
-		shape.draw(get_canvas_item(), Color(1, 0, 1, 0.5))
+		shape.draw(get_canvas_item(), Color(1, 0, 1, 0.35))
--- xt_hud.gd
+++ xt_hud.gd
@@ -1,7 +1,25 @@
-extends Label
+extends Control
+@onready var player = $"../../../World/EntityRoot/Player"
+@onready var xt_bar = $ProgressBar
+@onready var xt_label = $ProgressBar/Label
+@onready var prog_bar = $ProgressBar
+
+var xt_max: int
+var xt_current: int
 
 func _ready() -> void:
-	pass
+	xt_max = player.xt_max
+	xt_current = player.xt_current
+		
+	xt_bar.max_value = xt_max
+	xt_bar.value = xt_current
+	
+	prog_bar.value = xt_current
+	
+	xt_label.text = "%s / %s" % [xt_current, xt_max]
 
-func _on_cspg_xt_changed(new_value) -> void:
-	self.text = "XT: %s" % str(int(new_value))
+
+func _on_player_change_xt(new_value) -> void:
+	xt_current = new_value
+	xt_label.text = "%s / %s" % [xt_current, xt_max]
+	prog_bar.value = xt_current
